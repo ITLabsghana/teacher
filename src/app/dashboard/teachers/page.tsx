910506f3a9367c3ab961d3b449a1663c46232965
Tms Proj <tmsproject122@gmail.com>
2025-08-06 08:53:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/teachers/page.tsx
+++ src/app/dashboard/teachers/page.tsx
@@ -1,11 +1,6 @@
-
-"use client";
-
 import TeachersTab from '@/components/dashboard/teachers-tab';
 import { Suspense } from 'react';
 import { getSchools, getTeachers }from '@/lib/supabase';
-import type { Teacher, School } from '@/lib/types';
-
 
 export default async function TeachersPage() {
     const initialTeachers = await getTeachers(0, 20);
